47d701a9ebb1af8a480e215235ab02ab99369eb4
aristotelesbr <aristotelesbr2014@gmail.com>
2017-05-09 22:59:17 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <AppHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes"></AppHeader>
+    <AppNewQuote @quoteAdded="newQuote"></AppNewQuote>
+    <AppQuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></AppQuoteGrid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info. Click on quote to delete it!
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AppQuoteGrid from './components/QuoteGrid.vue'
+  import AppNewQuote from './components/NewQuote.vue'
+  import AppHeader from './components/Header.vue'
+
+  export default {
+   	data() {
+   		return {
+   			quotes: ['My first quote'],
+   			maxQuotes: 10,
+   		}
+    },
+    components: { AppQuoteGrid, AppNewQuote, AppHeader },
+    methods: {
+      newQuote(quote) {
+        if (this.quotes.length >= this.maxQuotes ) {
+          return alert('please delete quotes')
+        }
+        this.quotes.push(quote)
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="row">
+		<AppQuote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</AppQuote>
+	</div>
+</template>
+
+<script>
+	import AppQuote from './Quote.vue'
+	export default {
+	  name: 'QuoteGrid',
+	  props: ['quotes'],
+	  components: { AppQuote },
+	  methods: {
+	  	deleteQuote(index) {
+	  		this.$emit('quoteDeleted', index)
+	  	}
+	  }
+	}
+</script>
+
+<style>
+</style>
